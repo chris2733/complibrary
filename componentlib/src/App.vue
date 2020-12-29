e900fd68499f3ec4e56e94272c6ad40c39f2e848
Chris Law <claw@spindogs.com>
2020-12-29 22:02:38 +0000

    Proj setup, vue create, setting up sass

---
+++ componentlib/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <div class="topbar">
+      <h1 class="topbar-title">Component lib</h1>
+      <a href="#nogo">Buttons</a>
+      <a href="#nogo">Components</a>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+.topbar {
+  display: flex;
+  &-title {
+    flex-grow: 1;
+  }
+}
+</style>
