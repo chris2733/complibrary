c800f0e413b4fd5a8fe8353a8d2da237ce2eeb52
Chris Law <claw@spindogs.com>
2021-01-09 22:51:35 +0000

    Making colour picker component

--- componentlib/src/components/ButtonsPage.vue
+++ componentlib/src/components/ButtonsPage.vue
@@ -1,131 +1,143 @@
 <template>
-    <div class="buttonshead">
-    <p class="whitetext">{{ bezier.bezierVal }}</p>
-        <p class="whitetext">{{ bezier.speed }}</p>
-        <section class="buttonshead-panels">
-            <div class="buttonshead-panels-panel" v-if="showPanel == 'panel1'">
-                panel 1
-            </div>
-            <div class="buttonshead-panels-panel" v-if="showPanel == 'panel2'">
-                <bezier-gen @pass-bezier-values="updateBezier" :bezier-live-vals="bezier"></bezier-gen>
-            </div>
-            <div class="buttonshead-panels-panel" v-if="showPanel == 'panel3'">
-                panel 3
-            </div>
-        </section>
-        <section class="buttonshead-main">
-            <p>Buttons Page</p>
-            <div class="buttonshead-tabs">
-                <button
-                    class="buttonshead-tabs-each"
-                    @click="showHidePanel('panel1')"
-                >
-                    <img src="../assets/paintimg.png" alt="" />
-                </button>
-                <button
-                    class="buttonshead-tabs-each"
-                    @click="showHidePanel('panel2')"
-                >
-                    <img src="../assets/bounceimg.png" alt="" />
-                </button>
-                <button
-                    class="buttonshead-tabs-each"
-                    @click="showHidePanel('panel3')"
-                >
-                    <img src="../assets/textimg.png" alt="" />
-                </button>
-            </div>
-        </section>
-        <section class="buttonshead-textfill">
-            <p>Button Text</p>
-            <input type="text" v-model="buttonFillText" />
-        </section>
-        <buttons-list></buttons-list>
-    </div>
+	<div class="buttonshead">
+		<p class="whitetext">{{ bezier.bezierVal }}</p>
+		<p class="whitetext">{{ bezier.speed }}</p>
+        <p class="whitetext">{{ currentColour }}</p>
+		<section class="buttonshead-panels">
+			<div class="buttonshead-panels-panel" v-if="showPanel == 'panel1'">
+				<colour-picker
+					@pass-colour-values="updateColour"
+				></colour-picker>
+			</div>
+			<div class="buttonshead-panels-panel" v-if="showPanel == 'panel2'">
+				<bezier-gen
+					@pass-bezier-values="updateBezier"
+					:bezier-live-vals="bezier"
+				></bezier-gen>
+			</div>
+			<div class="buttonshead-panels-panel" v-if="showPanel == 'panel3'">
+				panel 3
+			</div>
+		</section>
+		<section class="buttonshead-main">
+			<p>Buttons Page</p>
+			<div class="buttonshead-tabs">
+				<button
+					class="buttonshead-tabs-each"
+					@click="showHidePanel('panel1')"
+				>
+					<img src="../assets/paintimg.png" alt="" />
+				</button>
+				<button
+					class="buttonshead-tabs-each"
+					@click="showHidePanel('panel2')"
+				>
+					<img src="../assets/bounceimg.png" alt="" />
+				</button>
+				<button
+					class="buttonshead-tabs-each"
+					@click="showHidePanel('panel3')"
+				>
+					<img src="../assets/textimg.png" alt="" />
+				</button>
+			</div>
+		</section>
+		<section class="buttonshead-textfill">
+			<p>Button Text</p>
+			<input type="text" v-model="buttonFillText" />
+		</section>
+		<buttons-list></buttons-list>
+	</div>
 </template>
 
 <script>
 import buttonsList from "./Buttons.vue";
 import bezierGenerator from "./BezierGenerator.vue";
+import colourPicker from "./ColourPicker.vue";
 
 export default {
-    components: {
-        "buttons-list": buttonsList,
-        'bezier-gen': bezierGenerator,
-    },
-    data() {
-        return {
-            buttonFillText: "Button",
-            showPanel: null,
-            bezier: {
-                bezierVal: [0.25,0.25,0.75,0.75],
-                speed: 300,
-            }
-        };
-    },
-    methods: {
-        showHidePanel(panelNo) {
-            if (this.showPanel == panelNo) {
-                this.showPanel = null;
-            } else {
-                this.showPanel = panelNo;
-            }
-        },
-        updateBezier(newvals) {
-            const newarr = [];
-            newvals.bezier.forEach(el => {
-                newarr.push(parseFloat(el));
-            });
-            this.bezier.bezierVal = newarr;
-            this.bezier.speed = parseInt(newvals.transition);
-        }
-    },
+	components: {
+		"buttons-list": buttonsList,
+		"bezier-gen": bezierGenerator,
+		"colour-picker": colourPicker,
+	},
+	data() {
+		return {
+			buttonFillText: "Button",
+			showPanel: null,
+			bezier: {
+				bezierVal: [0.25, 0.25, 0.75, 0.75],
+				speed: 300,
+			},
+			currentColour: "hsl(200, 85%, 50%)",
+		};
+	},
+	methods: {
+		showHidePanel(panelNo) {
+			if (this.showPanel == panelNo) {
+				this.showPanel = null;
+			} else {
+				this.showPanel = panelNo;
+			}
+		},
+		updateBezier(newvals) {
+			const newarr = [];
+			newvals.bezier.forEach((el) => {
+				newarr.push(parseFloat(el));
+			});
+			this.bezier.bezierVal = newarr;
+			this.bezier.speed = parseInt(newvals.transition);
+		},
+		updateColour(colourval) {
+			this.currentColour = colourval;
+		},
+	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .buttonshead {
-    color: #fff;
-    border-top: 1px solid #fff3;
-    &-main {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 0 0 20px;
-    }
-    &-tabs {
-        &-each {
-            position: relative;
-            display: inline-block;
-            height: 80px;
-            width: 80px;
-            transition: all 0.3s ease;
-            background: transparent;
-            border-left: 1px solid #fff3;
-            &:hover,
-            &:focus {
-                background: #383838;
-            }
-            img {
-                height: 25px;
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-            }
-        }
-    }
-    &-textfill {
-        display: flex;
-        p {
-            padding: 20px;
-            border-top: 1px solid #fff3;
-        }
-        input {
-            flex-grow: 1;
-            padding: 0 20px;
-        }
-    }
+	color: #fff;
+	border-top: 1px solid #fff3;
+	&-main {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 0 0 20px;
+	}
+	&-tabs {
+		&-each {
+			position: relative;
+			display: inline-block;
+			height: 80px;
+			width: 80px;
+			transition: all 0.3s ease;
+			background: transparent;
+			border-left: 1px solid #fff3;
+			&:hover,
+			&:focus {
+				background: #383838;
+			}
+			img {
+				height: 25px;
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+			}
+		}
+	}
+	&-textfill {
+		display: flex;
+		p {
+			padding: 20px;
+			border-top: 1px solid #fff3;
+		}
+		input {
+			flex-grow: 1;
+			padding: 0 20px;
+		}
+	}
 }
 </style>
---
+++ componentlib/src/components/ColourPicker.vue
@@ -0,0 +1,381 @@
+<template>
+	<section class="colourpicker" :style="cssVars">
+		<div class="colourpicker-demo"></div>
+		<div
+			class="colourpicker-swatch"
+			:style="currentSwatch"
+			@mousedown="swatchActive"
+			@mousemove="moveColourSwatch"
+			@mouseleave="leaveSwatch"
+			@mouseup="leaveSwatch"
+			id="swatchPickerWrap"
+		></div>
+		<div class="colourpicker-draggers">
+			<div class="colourpicker-draggers-colour">
+				<input
+					type="range"
+					min="0"
+					max="360"
+					:value="currentColourValues[0]"
+					@input="hueChange"
+				/>
+			</div>
+			<div class="colourpicker-draggers-opacity">
+				<input
+					type="range"
+					min="0"
+					max="255"
+					:value="currentColourValues[3]"
+					@input="opacityChange"
+				/>
+			</div>
+		</div>
+		<div class="colourpicker-customhex">
+			<input type="text" placeholder="HEX CODE" maxlength="6" @focusout="checkCustomHex" :class="{ error: customHexError }"/>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	emits: {
+		"pass-colour-values": function(vals) {
+			if (vals) {
+				return true;
+			} else {
+				console.log("no colour vals passed");
+				return false;
+			}
+		},
+	},
+	data() {
+		return {
+			// had to use HSV from the user, convert to rgb, then to hex to be consistent
+			currentColourValues: [239, 54, 81, 255],
+			currentColour: "#5e60ce",
+			swatchWidth: 0,
+			swatchHeight: 0,
+			swatchPosition: {
+				x: "50%",
+				y: "50%",
+			},
+			theme: {
+				border: "#fff",
+			},
+			swatchPickActive: false,
+            customHexError: false,
+		};
+	},
+	computed: {
+		cssVars() {
+			return {
+				"--current-colour": this.currentColour,
+				"--borders": this.theme.border,
+				"--swatchx": this.swatchPosition.x,
+				"--swatchy": this.swatchPosition.y,
+			};
+		},
+		currentSwatch() {
+			return {
+				background:
+					"linear-gradient(to bottom, transparent 0%, #000 100%), linear-gradient(to right, #fff 0%, " +
+					this.currentColour +
+					" 100%)",
+			};
+		},
+		moveSwatchPicker() {
+			// console.log(this.swatchPosition.x);
+			return {
+				// bottom: this.swatchPosition.y,
+				left: this.swatchPosition.x,
+			};
+		},
+	},
+	mounted() {
+		this.setSwatchWidth();
+	},
+	methods: {
+		setSwatchWidth() {
+			const swatchEl = document.querySelector("#swatchPickerWrap");
+			if (swatchEl) {
+				this.swatchWidth = document.querySelector(
+					"#swatchPickerWrap"
+				).offsetWidth;
+				this.swatchHeight = document.querySelector(
+					"#swatchPickerWrap"
+				).offsetHeight;
+			}
+		},
+		opacityChange() {
+			this.currentColourValues[3] = Number(event.target.value);
+			this.colourChange();
+		},
+		hueChange() {
+			this.currentColourValues[0] = event.target.value;
+			this.colourChange();
+		},
+		colourChange() {
+			const rgbval = this.HSVtoRGB(
+                this.currentColourValues[0],
+                this.currentColourValues[1],
+                this.currentColourValues[2]
+            );
+            this.RGBAtoAHEX(rgbval);
+			this.$emit("pass-colour-values", this.currentColour);
+		},
+		RGBAtoAHEX(rgbval) {
+            let hexval = '';
+            rgbval.forEach(rgbval => {
+                let hex = rgbval.toString(16);
+                hexval = hexval + (hex.length == 1 ? "0" + hex : hex).toString();
+            });
+            // add opacity here, after hsv to rgb calc
+            hexval += this.currentColourValues[3].toString(16);
+            this.currentColour = '#' + hexval;
+		},
+		swatchActive() {
+			this.swatchPickActive = true;
+		},
+		moveColourSwatch(e) {
+			if (this.swatchPickActive === true) {
+				const offsetX = e.offsetX;
+				const offsetY = e.offsetY;
+				const pickerX = (offsetX / this.swatchWidth) * 100;
+				const pickerY =
+					((offsetY / this.swatchHeight) * 100 - 100) * -1;
+				this.swatchPosition.x = pickerX + "%";
+				this.swatchPosition.y = pickerY + "%";
+				this.currentColourValues[1] = pickerX;
+				this.currentColourValues[2] = pickerY;
+                this.colourChange();
+
+			}
+		},
+		leaveSwatch() {
+			this.swatchPickActive = false;
+		},
+		HSVtoRGB(h, s, v) {
+			var r, g, b;
+			var i;
+			var f, p, q, t;
+
+			// Make sure our arguments stay in-range
+			h = Math.max(0, Math.min(360, h));
+			s = Math.max(0, Math.min(100, s));
+			v = Math.max(0, Math.min(100, v));
+
+			// We accept saturation and value arguments from 0 to 100 because that's
+			// how Photoshop represents those values. Internally, however, the
+			// saturation and value are calculated from a range of 0 to 1. We make
+			// That conversion here.
+			s /= 100;
+			v /= 100;
+
+			if (s == 0) {
+				// Achromatic (grey)
+				r = g = b = v;
+				return [
+					Math.round(r * 255),
+					Math.round(g * 255),
+					Math.round(b * 255),
+				];
+			}
+
+			h /= 60; // sector 0 to 5
+			i = Math.floor(h);
+			f = h - i; // factorial part of h
+			p = v * (1 - s);
+			q = v * (1 - s * f);
+			t = v * (1 - s * (1 - f));
+
+			switch (i) {
+				case 0:
+					r = v;
+					g = t;
+					b = p;
+					break;
+
+				case 1:
+					r = q;
+					g = v;
+					b = p;
+					break;
+
+				case 2:
+					r = p;
+					g = v;
+					b = t;
+					break;
+
+				case 3:
+					r = p;
+					g = q;
+					b = v;
+					break;
+
+				case 4:
+					r = t;
+					g = p;
+					b = v;
+					break;
+
+				default:
+					// case 5:
+					r = v;
+					g = p;
+					b = q;
+			}
+
+			return [
+				Math.round(r * 255),
+				Math.round(g * 255),
+				Math.round(b * 255),
+			];
+		},
+        checkCustomHex() {
+            let regex = /^(?:[0-9a-fA-F]{3}){1,2}$/g;
+            let regtest = regex.test(event.target.value);
+            if (regtest !== true && event.target.value.length !== 0) {
+                this.customHexError = true;
+            } else {
+                this.customHexError = false;
+            }
+            console.log(this.customHexError);
+        },
+	},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.colourpicker {
+	border: 1px solid var(--borders);
+	border-radius: 10px;
+	padding: 20px;
+	width: 250px;
+	&-demo {
+		height: 100px;
+		background: var(--current-colour);
+	}
+	&-swatch {
+		position: relative;
+		width: 100%;
+		height: 100px;
+		margin: 0 0 15px 0;
+		background: linear-gradient(to bottom, transparent 0%, #000 100%),
+			linear-gradient(to right, #fff 0%, rgb(39, 199, 18) 100%);
+		&::before {
+			content: "";
+			height: 20px;
+			width: 20px;
+			position: absolute;
+			bottom: var(--swatchy);
+			left: var(--swatchx);
+			z-index: 1;
+			border: 3px solid #fff;
+			border-radius: 50%;
+			transform: translate(-50%, 50%);
+		}
+	}
+	&-draggers {
+		width: 100%;
+		margin: 0 0 15px 0;
+		&-colour input,
+		&-opacity input {
+			width: 100%;
+			-webkit-appearance: none;
+			height: 10px;
+			border-radius: 5px;
+			&::-webkit-slider-thumb,
+			&::-moz-range-thumb {
+				-webkit-appearance: none;
+				appearance: none;
+				width: 45px;
+				height: 45px;
+				border: 2px solid #fff;
+				cursor: pointer;
+			}
+		}
+		&-colour input {
+			background: linear-gradient(
+				to right,
+				hsl(0, 100%, 50%),
+				hsl(60, 100%, 50%),
+				hsl(120, 100%, 50%),
+				hsl(180, 100%, 50%),
+				hsl(240, 100%, 50%),
+				hsl(300, 100%, 50%),
+				hsl(360, 100%, 50%)
+			);
+		}
+		&-opacity {
+			position: relative;
+			z-index: 1;
+			&::before {
+				content: "";
+				position: absolute;
+				top: 55%;
+				left: 0;
+				height: 10px;
+				width: 100%;
+				background-image: linear-gradient(
+						45deg,
+						#ccc 25%,
+						transparent 25%
+					),
+					linear-gradient(-45deg, #ccc 25%, transparent 25%),
+					linear-gradient(45deg, transparent 75%, #ccc 75%),
+					linear-gradient(-45deg, transparent 75%, #ccc 75%);
+				background-size: 12px 12px;
+				background-position: 0 0, 0 6px, 6px -6px, -6px 0px;
+				z-index: -1;
+				border-radius: 5px;
+				transform: translateY(-50%);
+			}
+			input {
+				background: linear-gradient(
+					to right,
+					transparent 0%,
+					#000 100%
+				);
+			}
+		}
+	}
+	&-customhex {
+		input {
+			width: 100%;
+			background: none;
+			outline: none;
+			box-shadow: none;
+			border: 1px solid var(--borders);
+			text-align: center;
+			font-size: 12px;
+			letter-spacing: 2px;
+			color: #fff;
+			border-radius: 10px;
+			padding: 7px 0;
+            transition: all 0.1s ease;
+			$placeholdercolour: #fff;
+			&::-webkit-input-placeholder {
+				/* Chrome/Opera/Safari */
+				color: placeholdercolour;
+			}
+			&::-moz-placeholder {
+				/* Firefox 19+ */
+				color: placeholdercolour;
+			}
+			&:-ms-input-placeholder {
+				/* IE 10+ */
+				color: placeholdercolour;
+			}
+			&:-moz-placeholder {
+				/* Firefox 18- */
+				color: placeholdercolour;
+			}
+            &.error {
+                border: 1px solid red;
+            }
+		}
+	}
+}
+</style>
